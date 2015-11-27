b03130904a8d6baafe593fc063d1c1e9ba7769b3
Floste <floste@kerflo.fr>
2015-11-27 10:04:56 +0100

    Initialisation du projet, frontend operationnel

---
+++ public/js/scoreSheetArchery.js
@@ -0,0 +1,105 @@
+$().ready(function(){
+    /*
+    Load step
+    */
+    if (typeof (Storage) == "undefined") {
+        //WebStorage not supported.
+        alert("WebStorage not supported");
+    } else {
+        localStorage.setItem("series", {});
+        if (localStorage.getItem("volees") === undefined || !isValidStorageVolees()) {
+            newSerie();
+        } else {
+            updateTotal();
+            updateVolees();
+        }
+        clearSaisie();
+    }
+    
+    /*
+    	Actions boutons
+    */
+    $("#saisie .pts").click(function () {
+        for (i = 1; i <= 3; i++) {
+            cur_elt = $("#saisie_volee input[name=fleche_" + i + "]");
+            if ($(cur_elt).val()=="") {
+                $(cur_elt).val($(this).html());
+                break;
+            }
+        }
+    });
+    $("#clearButton").click(function () {
+        clearSaisie();
+    });
+    $("#NewSerieButton").click(function () {
+        newSerie();
+    });
+    $("#okVoleeButton").click(function () {
+        var data = JSON.parse(localStorage.getItem("volees"));
+        data.volees.push(new Array(
+        0 + $("#saisie_volee input[name=fleche_1]").val(),
+        0 + $("#saisie_volee input[name=fleche_2]").val(),
+        0 + $("#saisie_volee input[name=fleche_3]").val()));
+        console.log(data);
+        localStorage.setItem("volees", JSON.stringify(data));
+        clearSaisie();
+        updateTotal();
+        updateVolees();
+    });
+    
+});
+/*
+	Fonctions outils
+*/
+function newSerie() {
+    localStorage.setItem("volees", '{"volees":[]}');
+    $("#saisie").removeClass("disabled");
+    clearSaisie();
+    updateTotal();
+    updateVolees();
+}
+
+function clearSaisie() {
+    for (i = 1; i <= 3; i++) {
+        $("#saisie_volee input[name=fleche_" + i + "]").val("");
+    }
+}
+
+function updateTotal() {
+    var data = JSON.parse(localStorage.getItem("volees"));
+    total = 0;
+    for (i = 0; i < data.volees.length; i++) {
+        total += parseInt(data.volees[i][0]);
+        total += parseInt(data.volees[i][1]);
+        total += parseInt(data.volees[i][2]);
+    }
+    $("#total").html(total);
+}
+
+function updateVolees() {
+    $("#volees").html("");
+    var data = JSON.parse(localStorage.getItem("volees"));
+    for (i = 0; i < data.volees.length; i++) {
+        str = "<div class='volee'>" +
+            "<span class='num_volee'>" + (i + 1) + "</span>";
+        for (j = 0; j < 3; j++) {
+            str += "<span class='fleche'>" + parseInt(data.volees[i][j]) + "</span>";
+        }
+        $("#volees").append(str);
+        $("#volees").scrollTop(9999);
+    }
+    //Controle, si on est à 10 volées, on desactive la saisie
+    if (data.volees.length >= 10) {
+        $("#saisie").addClass("disabled");
+    }
+}
+
+function isValidStorageVolees(){
+    var data = JSON.parse(localStorage.getItem("volees"));
+    if(data){
+		if(data.volees){
+        	return true;
+        }
+    }
+	return false;
+}
