7da625000893511f2985b4f040a64a7668b59567
Floste <floste@kerflo.fr>
2016-10-21 15:04:00 +0200

    Ajout couleur stats sur la saisie des scores

--- public/js/scoreSheetArchery.js
+++ public/js/scoreSheetArchery.js
@@ -89,6 +89,7 @@ function updateTotal() {
 function updateVolees() {
     $("#volees").html("");
     var data = JSON.parse(localStorage.getItem("volees"));
+    var TabNbColor = {"jaune":0, "rouge":0, "bleu":0, "noir":0, "blanc":0, "nbFlechesTirees":0};
     var valFleche;
     for (i = 0; i < data.volees.length; i++) {
         str = "<div class='volee'>" +
@@ -97,10 +98,37 @@ function updateVolees() {
             valFleche = "";
             valFleche += parseInt(data.volees[i][j]);
             
+            TabNbColor["nbFlechesTirees"] += 1;
+            switch (valFleche) {
+                case '10':TabNbColor["jaune"] += 1;
+                    break;
+                case '9':TabNbColor["jaune"] += 1;
+                    break;
+                case '8':TabNbColor["rouge"] += 1;
+                    break;
+                case '7':TabNbColor["rouge"] += 1;
+                    break;
+                case '6':TabNbColor["bleu"] += 1;
+                    break;
+                case '5':TabNbColor["bleu"] += 1;
+                    break;
+                case '4':TabNbColor["noir"] += 1;
+                    break;
+                case '3':TabNbColor["noir"] += 1;
+                    break;
+                case '2':TabNbColor["blanc"] += 1;
+                    break;
+                case '1':TabNbColor["blanc"] += 1;
+                    break;
+                case '0':TabNbColor["blanc"] += 1;
+                    break;
+            }
+            
             str += "<span class='fleche val"+valFleche+"'>" + valFleche + "</span>";
         }
         $("#volees").append(str);
         $("#volees").scrollTop(9999);
+        updateStatColors(TabNbColor);
     }
     //Controle, si on est à NB_VOLEES_PAR_SERIE volées, on desactive la saisie
     //La série est finie, on propose de l'enregistrer
@@ -109,6 +137,42 @@ function updateVolees() {
         $("#saisie").addClass("disabled");
         $("#saveSerie").removeClass("disabled");
         $("#saveSerie").addClass("enabled");
+    }
+}
+
+function updateStatColors(tabFlechesTriees){
+    if(tabFlechesTriees["nbFlechesTirees"]>0){
+        var colorJaune = "#FBD400";
+        var colorRouge = "#D91919";
+        var colorBleu = "#009ECE";
+        var colorNoir = "#000000";
+        var colorBlanc = "#ffffff";
+        
+        pctJaune = parseInt((tabFlechesTriees["jaune"] * 100) / tabFlechesTriees["nbFlechesTirees"]);
+        pctRouge = parseInt((tabFlechesTriees["rouge"] * 100) / tabFlechesTriees["nbFlechesTirees"]);
+        pctBleu = parseInt((tabFlechesTriees["bleu"] * 100) / tabFlechesTriees["nbFlechesTirees"]);
+        pctNoir = parseInt((tabFlechesTriees["noir"] * 100) / tabFlechesTriees["nbFlechesTirees"]);
+        pctBlanc = parseInt((tabFlechesTriees["blanc"] * 100) / tabFlechesTriees["nbFlechesTirees"]);
+        
+        initPctRouge = pctJaune;
+        pctRouge += initPctRouge;
+        initPctBleu = pctRouge;
+        pctBleu += initPctBleu;
+        initPctNoir = pctBleu;
+        pctNoir += initPctNoir;
+        initPctBlanc = pctNoir;
+        pctBlanc = 100;
+    
+        strJaune = colorJaune + " 0%, " + colorJaune + " " + pctJaune + "%, ";
+        strRouge = colorRouge + " " + initPctRouge + "%, " + colorRouge + " " + pctRouge + "%, ";
+        strBleu = colorBleu + " " + initPctBleu + "%, " + colorBleu + " " + pctBleu + "%, ";
+        strNoir = colorNoir + " " + initPctNoir + "%, " + colorNoir + " " + pctNoir + "%, ";
+        strBlanc = colorBlanc + " " + initPctBlanc + "%, " + colorBlanc + " " + pctBlanc + "% ";
+    
+        bgColor = strJaune + strRouge + strBleu + strNoir + strBlanc;
+        bgColor = "linear-gradient(to right," + bgColor + ")";
+    
+        $("#statColors").css("background-image",bgColor);
     }
 }
 
